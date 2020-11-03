a5c156872f803babf07180bec610ab456faf343d
Matthew Nte <Matthewnte1@gmail.com>
2020-11-03 23:11:38 +0100

    ft(style-homepage): add banner photo

---
+++ pages/components/Appbar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  title: {
+    flexGrow: 1,
+  },
+  btn: {
+    textTransform: 'none',
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent" elevation={0} style={{ color: "#fff" }}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Smart autos
+          </Typography>
+          <Button color="inherit" className={classes.btn}>
+            Become a dealer
+          </Button>
+          <Button color="inherit" className={classes.btn}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import withAuthentication from "../utils/withAuthentication";
-import useCustomHook from "../hooks/useCustomHook";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import AppBar from './components/Appbar';
+import ChooseCategoryCard from './components/ChooseCategoryCard';
+import withAuthentication from '../utils/withAuthentication';
+import useCustomHook from '../hooks/useCustomHook';
 
 const Home = () => {
   const { name } = useCustomHook();
+
   return (
     <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <img src="bg-car-md.jpg" alt="Picture of a car" width="100%" />
-        {/* <section className={styles.container}>
+      <main className="text-gray-900">
+        <section className={styles.backgroundImage}>
+          <AppBar />
+          <section className={styles.container}>
+            <ChooseCategoryCard />
+          </section>
+        </section>
+        <section className={styles.container}>
           <h1 className="text-center font-bold text-black text-6xl">{name}</h1>
-        </section> */}
+        </section>
       </main>
     </>
   );
